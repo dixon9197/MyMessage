a0e361fb486c5e48de8f90a8c7e97b056084d871
dixon <dixon@mym.local>
2016-06-29 11:34:18 +0800

    Initial Commit

---
+++ MyMessage_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyMessage_OC
+//
+//  Created by dixon on 16/6/29.
+//  Copyright © 2016年 Monaco1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyMessage_OC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyMessage_OC
+//
+//  Created by dixon on 16/6/29.
+//  Copyright © 2016年 Monaco1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
